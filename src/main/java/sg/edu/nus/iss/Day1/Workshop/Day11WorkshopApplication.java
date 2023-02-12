99fba3b60c3d96daeeed1ce74db4b92fec9021cf
shiyun-ho <serene.hoshiyun@gmail.com>
2023-02-12 21:06:40 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/Day1/Workshop/Day11WorkshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.Day1.Workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day11WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/Day1/Workshop/Day11WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.Day1.Workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day11WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
